b80d768a6b624a257413d84f75e29903f79fbc0a
CodingFlops <68713163+CodingFlops@users.noreply.github.com>
2025-09-02 12:50:09 -0700

    Add files via upload

---
+++ SwiftTests/DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Florian Wessels on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
